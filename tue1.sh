a144baa48b33cd9268ef7af416af73cc54797b06
sangmeshgit <109062970+sangmeshgit@users.noreply.github.com>
2022-11-27 23:36:07 +0530

    Update tue1.sh

--- tue1.sh
+++ tue1.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-echo we will find greatest of any four number
-echo enter 1st number
-read $1
-echo enter 2nd number
-read $2
-echo enter 3rd number
-read $3
-echo enter 4th number
-read $4
-if [ $1 -ge $2 -a $1 -ge $3 -a $a -ge $4 ]
+echo we are finding greatest of above four numbers
+a=$1
+b=$2
+c=$3
+d=$4
+if [ $a -ge $b -a $a -ge $c -a $1 -ge $d ]
 then
-	echo "$a is the greatest number among 4 entered number"
-elif [ $2 -ge $1 -a $2 -ge $3 -a $2 -ge $4 ]
+	echo "$a is the greatest number among above 4 entered number"
+elif [ $b -ge $a -a $b -ge $c -a $b -ge $d ]
 then
-	echo "$b is greatest number among 4 entered number"
-elif [ $3 -ge $1 -a $3 -ge $2 -a $3 -ge $4 ]
+	echo "$b is greatest number among above 4 entered number"
+elif [ $c -ge $a -a $c -ge $b -a $c -ge $d ]
 then
-	echo "$3 is greatest number among 4 entered number"
+	echo "$c is greatest number among above 4 entered number"
 else
-	"echo $4 is greatest number among 4 entered number"
+	echo "$d is greatest number among above 4 entered number"
 fi
-
